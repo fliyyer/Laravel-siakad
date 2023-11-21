a0a60c6d0509f86b8c5cefe36b1315ef05b2c94f
rahmatraywhite <rahmat.hidayat@raywhite.co.id>
2023-11-21 14:04:21 +0700

    test

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -322,8 +322,8 @@ class ComposerStaticInitbfd12640229ead28b81a3850ca839aca
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'f4c7a6b77635c864ad05084bdf8f8ab99e422f07',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a4e5f6746ac705b43e6932f58e1adaf383b6cff0',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -365,9 +365,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'f4c7a6b77635c864ad05084bdf8f8ab99e422f07',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a4e5f6746ac705b43e6932f58e1adaf383b6cff0',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -721,8 +721,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0',
             ),
         ),
         'psr/simple-cache' => array(
